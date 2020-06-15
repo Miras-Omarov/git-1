81bfd3cf67892cd6257b9dd04c2d13dc9c776f80
Miras <mikosia@bk.ru>
2020-06-16 00:28:20 +0600

    CRUD v2.0 -Option UPDATE added -Option DELETE added -Create pattern was changed

--- .idea/src/com/github/mo/CRUD1.java
+++ .idea/src/com/github/mo/CRUD1.java
@@ -1,12 +1,12 @@
 package com.github.mo;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CRUD1 {
-    private static String mode;
-    private static boolean isFileFound = true;
-    private static boolean isReady = false;
+    private static String mode,path;
+    private static boolean isFileFound = true,isReady = false;
     private static final Scanner scanner = new Scanner(System.in);
     private static BufferedReader bufferedReader = null;
     private static BufferedWriter bufferedWriter = null;
@@ -15,38 +15,47 @@ public class CRUD1 {
         start();
         if(isFileFound) {
             switch(mode.toLowerCase()) {
-                case "create": int id = read();
+                case "create":
+                    int id = read();
                     create(id);
                     break;
+                case "update":
+                    update();
+                    break;
+                case "delete":
+                    delete();
+                    break;
                 default:
-                    System.out.println("Not supported in this version.(v1.0)");
+                    System.out.println("Something went wrong..");
             }
+            bufferedWriter.close();
+            bufferedReader.close();
+            scanner.close();
         }
-        bufferedWriter.close();
-        bufferedReader.close();
-        scanner.close();
     }
 
     public static void start() {
         System.out.println("Input a file path...");
-        String path = scanner.nextLine();
+        path = scanner.nextLine();
         try {
             bufferedReader = new BufferedReader(new FileReader(path));
             bufferedWriter = new BufferedWriter(new FileWriter(path,true));
         } catch(FileNotFoundException e) {
             System.out.println("File was not found.");
             isFileFound = false;
+            return;
         } catch(IOException e) {
             System.out.println(e.getMessage());
             isFileFound = false;
+            return;
         }
-        System.out.println("!!!AT THE MOMENT ONLY CREATE IS AVAILABLE!!!\nWhat would you like to do?\nCreate\nUpdate\nDelete");
+        System.out.println("What would you like to do?\nCreate\nUpdate\nDelete");
         mode = scanner.nextLine();
     }
 
     private static int read() throws IOException {
         int maxId = 0,id;
-        while(bufferedReader.ready()) {
+        while(bufferedReader.ready()) { // Finds the max ID in the file
             isReady = true;
             String line = bufferedReader.readLine();
             id = Integer.parseInt(line.substring(0,8).trim());
@@ -56,15 +65,15 @@ public class CRUD1 {
     }
 
     private static void create(int id) throws IOException {
-        System.out.println("productName:quantity:price");
-        System.out.println("NOTE:Product name cannot be more than 30 letters,whereas quantity cannot be more than 8 digits.\nInput the product name, quantity, and price as shown.");
+        System.out.println("productName:price:quantity");
+        System.out.println("NOTE:Product name cannot be more than 30 letters,whereas price cannot be more than 8 digits.\nInput the product name, price, and quantity as shown.");
 
         String productString = scanner.nextLine();
         String[] product = productString.split(":");
         String productId = String.valueOf(id);
         String productName = product[0];
-        String productQuantity = product[1];
-        String productPrice = product[2];
+        String productPrice = product[1];
+        String productQuantity = product[2];
 
         while(productId.length() < 8) {
             productId += " ";
@@ -72,14 +81,85 @@ public class CRUD1 {
         while(productName.length() < 30) {
             productName += " ";
         }
-        while(productQuantity.length() < 8) {
-            productQuantity += " ";
+        while(productPrice.length() < 8) {
+            productPrice += " ";
         }
 
-        String line = productId + productName + productQuantity + productPrice;
+        String line = productId + productName + productPrice + productQuantity;
         if(isReady) {
             bufferedWriter.write("\n");
         }
         bufferedWriter.write(line);
+        System.out.println("The product has been successfully created.");
+    }
+
+    public static void update() throws IOException {
+        System.out.println("Enter ID");
+        String productID = scanner.nextLine();
+        System.out.println("Enter the new product(productName:price:quantity)");
+        String[] newProduct = scanner.nextLine().split(":");
+        String productName = newProduct[0];
+        String productPrice = newProduct[1];
+        String productQuantity = newProduct[2];
+        while(productName.length() < 30){
+            productName += " ";
+        }
+        while(productPrice.length() < 8){
+            productPrice += " ";
+        }
+        ArrayList<String> list = new ArrayList<>();
+
+        while(bufferedReader.ready()) {
+            String line = bufferedReader.readLine();
+            String id = line.substring(0,8);
+            if(id.trim().equals(productID)) {
+                line = "";
+                line += id + productName + productPrice + productQuantity;
+            }
+            list.add(line);
+        }
+        bufferedReader.close();
+        BufferedWriter bufferedWriter2 = new BufferedWriter(new FileWriter(path));
+        bufferedWriter2.write("");
+        bufferedWriter2.close();
+        int alert = 0;
+        for(String line : list) {
+            bufferedWriter.write(line);
+            if(alert == list.size() - 1){}
+            else
+                bufferedWriter.write("\n");
+            alert++;
+        }
+        bufferedWriter.close();
+        scanner.close();
+        System.out.println("The product has been successfully updated.");
+    }
+
+    public static void delete() throws IOException {
+        System.out.println("Enter the product ID..");
+        String productID = scanner.nextLine();
+        ArrayList<String> list = new ArrayList<>();
+        while(bufferedReader.ready()) {
+            String line = bufferedReader.readLine();
+            String id = line.substring(0,8);
+            if(id.trim().equals(String.valueOf(productID))) {
+            }
+            else list.add(line);
+        }
+        bufferedReader.close();
+        BufferedWriter bufferedWriter2 = new BufferedWriter(new FileWriter(path));
+        bufferedWriter2.write("");
+        bufferedWriter2.close();
+        int alert = 0;
+        for(String line : list) {
+            bufferedWriter.write(line);
+            if(alert == list.size() - 1){}
+            else
+                bufferedWriter.write("\n");
+            alert++;
+        }
+        bufferedWriter.close();
+        scanner.close();
+        System.out.println("The product has been successfully deleted.");
     }
 }
